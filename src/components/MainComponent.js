b21fb95ecd00dd60cdaedb9c1191a5dc88129e97
Shalitha Pathiranage <shali96shri@gmail.com>
2020-05-10 23:01:00 +0530

    initial setup

---
+++ src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+
+
+class Main extends Component{
+
+
+    render(){
+        return(
+            <div>
+                Hello Bitches 
+            </div>
+        )
+    }
+};
+
+
+export default Main;
