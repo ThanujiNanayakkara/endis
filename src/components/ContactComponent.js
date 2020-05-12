f2871eeb44df03a04a53f6c49983f77c441d9785
Shalitha Pathiranage <shali96shri@gmail.com>
2020-05-12 16:25:54 +0530

    contact updated with carousel. bootstrap added

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Breadcrumb, BreadcrumbItem, Button,  Label,  Col, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
-
+import {Carousel} from 'react-bootstrap';
 
 class Contact extends Component{
 
@@ -14,9 +14,48 @@ class Contact extends Component{
                     <Breadcrumb>
                         <BreadcrumbItem><Link to= '/home'>Home</Link ></BreadcrumbItem>
                         <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-                    </Breadcrumb>
-                   
+                    </Breadcrumb>  
                 </div>
+                <Carousel >
+                        <Carousel.Item>
+                        <img
+                            className="d-block w-100 h-100"
+                            src="assets/images/mailbox.jpg"
+                            alt="First slide"
+                            style={{height : 100}}
+                            />
+                            <Carousel.Caption>
+                                <div className="col-12 col-sm-4 offset-sm-1">
+                                    <h5>Our Address</h5>
+                                    <address>
+                                    University of Moratuwa<br />
+                                    Katubedda, Moratuwa<br />
+                                    Sri Lanka<br />
+                                    <i className="fa fa-phone"></i>: +94711608162<br />
+                                    <i className="fa fa-fax"></i>: +94716853273<br />
+                                    <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">endis@gmail.com</a>
+                                    </address>
+                                </div>
+                            </Carousel.Caption>       
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 h-100"
+                                src="assets/images/social.png"
+                                alt="First slide"
+                                style={{height : 100}}
+                                />
+                            <Carousel.Caption>
+                                <div className="col-12 col-sm-11 offset-sm-1">
+                                    <div className="btn-group" role="group">
+                                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                                        <a role="button" className="btn btn-info" href="skype:echo123?call">><i className="fa fa-skype"></i> Skype</a>
+                                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                                    </div>
+                                </div>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 <div className="row row-content">
                     <div className="col-12">
                     <h3>Location Information</h3>
@@ -24,12 +63,12 @@ class Contact extends Component{
                     <div className="col-12 col-sm-4 offset-sm-1">
                             <h5>Our Address</h5>
                             <address>
-                            121, Clear Water Bay Road<br />
-                            Clear Water Bay, Kowloon<br />
-                            HONG KONG<br />
-                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            University of Moratuwa<br />
+                            Katubedda, Moratuwa<br />
+                            Sri Lanka<br />
+                            <i className="fa fa-phone"></i>: +94711608162<br />
+                            <i className="fa fa-fax"></i>: +94716853273<br />
+                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">endis@gmail.com</a>
                             </address>
                     </div>
                     <div className="col-12 col-sm-6 offset-sm-1">
